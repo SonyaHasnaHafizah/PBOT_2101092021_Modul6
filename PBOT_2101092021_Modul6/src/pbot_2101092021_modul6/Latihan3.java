caf95f71e606365286caca5d9e8130e761f697a8
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2022-11-10 10:27:01 +0700

    Create Latihan3.java

---
+++ PBOT_2101092021_Modul6/src/pbot_2101092021_modul6/Latihan3.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbot_2101092021_modul6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author SONYA HASNA HAFIZAH
+ */
+public class Latihan3 {
+    public static void main (String[] args){
+        //BufferedReader
+        System.out.println("-->Program Perpangkatan<--");
+        BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+      
+        String nama = "";
+        int pangkat=100;
+        int i = 1;
+        int sum=1;
+      
+      
+        try{
+            System.out.print("Masukkan Nama : ");
+            nama = dataIn.readLine();
+            
+            System.out.print("\nWhile-Loop\n");
+            i=1;
+            while(i<=pangkat){
+                System.out.print(sum + "."+ nama + " ");
+                sum=sum+1;
+            i++;
+            }
+            
+            sum=1;
+            System.out.print("\n\nDo-While\n");
+            i=1;
+            do{
+                System.out.print(sum + "."+ nama + " ");
+                sum=sum+1;
+            i++;
+            }while(i<=pangkat);
+      
+            sum=1;
+            System.out.print("\n\nFor-Loop\n");
+            for(i=1;i<=pangkat;i++){
+		System.out.print(sum + "."+ nama + " ");
+                sum=sum+1;
+            }  
+        }catch(IOException e){
+            System.out.println("Error!");
+        }
+    }
+}
